dd2ff992906f7cec84a47c7ed4205de407b6fabc
Sean Gay <sean.gay@gmail.com>
2024-09-30 18:06:06 +1000

    create WishItem data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  items = [
+    new WishItem('To learn angular'),
+    new WishItem('Get coffee', true),
+    new WishItem('Find grass that cuts itself'),
+  ];
   title = 'Banana';
 }

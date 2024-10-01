0ea69a6e2b73ab03696c1a7971420c3370b137f2
Sean Gay <sean.gay@gmail.com>
2024-10-01 11:12:17 +1000

    bind events to components
    - use round brackets "(" to bind to events
    - generally call a function on the component in order to perform an action
    - within the function manipulate what is required for the specific action being taken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,9 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself'),
   ];
   title = 'Banana';
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }

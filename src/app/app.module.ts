358f8ba8f1b2318331cfda4a1e07ccf8408e398c
Sean Gay <sean.gay@gmail.com>
2024-10-01 15:49:41 +1000

    issuing http requests
    - create WishService and inject into App.
    - used the ng create service command to generate the required files for the service
    - pull initial data from a json file via a http request in the init method of the AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -16,7 +17,8 @@ import { WishListItemComponent } from './wish-list-item/wish-list-item.component
     WishFilterComponent,
     WishListItemComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+
   providers: [],
   bootstrap: [AppComponent],
 })
